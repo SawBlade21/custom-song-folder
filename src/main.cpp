57b50bc5da94e68b51aeac012422ac20acf0132a
Sawblade <176694100+SawBlade21@users.noreply.github.com>
2025-06-06 16:23:57 -0400

    Update customIsSongDownloaded()
    now checks for both .mp3 and .ogg
    also added test hooks for mobile

--- src/main.cpp
+++ src/main.cpp
@@ -8,24 +8,41 @@ using namespace geode::prelude;
 class $modify(MusicDownloadManager) {
 
     gd::string pathForSongFolder(int p0) {
+        log::debug("pathForSongFolder()");
         std::filesystem::path customPath = Mod::get()->getSettingValue<std::filesystem::path>("custom-folder").string() + "\\";
         if (std::filesystem::exists(customPath)) return customPath.string();
         else return MusicDownloadManager::pathForSongFolder(p0);
     }
 
     gd::string pathForSFXFolder(int p0) {
+        log::debug("pathForSFXFolder()");
         std::filesystem::path customPath = Mod::get()->getSettingValue<std::filesystem::path>("custom-folder").string() + "\\";
         if (std::filesystem::exists(customPath)) return customPath.string();
         else return MusicDownloadManager::pathForSFXFolder(p0);
     }
+
+    gd::string pathForSong(int p0) {
+        log::debug("pathForSong()");
+        return MusicDownloadManager::pathForSong(p0);
+    }
+
+    gd::string pathForSFX(int p0) {
+        log::debug("pathForSFX()");
+        return MusicDownloadManager::pathForSFX(p0);
+    }
    
     bool customIsSongDownloaded(int id, std::filesystem::path songPath) {
-        std::string songID = std::to_string(id) + ".mp3";
-
+        
         if (!std::filesystem::is_directory(songPath)) return false;
 
+        std::string songID = std::to_string(id);
+        
         for (const auto& entry : std::filesystem::directory_iterator(songPath)) {
-            if (entry.is_regular_file() && entry.path().filename() == songID && !blacklistedFiles.contains(entry.path().filename().string()))
+
+            std::string name = entry.path().filename().string();
+            std::string ext = entry.path().extension().string();
+
+            if (entry.is_regular_file() && (ext == ".mp3" || ext == ".ogg") && name == (songID + ext) && !blacklistedFiles.contains(name))
                 return true;
         }
 
@@ -40,8 +57,10 @@ class $modify(MusicDownloadManager) {
         CCObject* obj;
         CCARRAY_FOREACH(songs, obj) {
             SongInfoObject* info = static_cast<SongInfoObject*>(obj);
-            if (info && customIsSongDownloaded(info->m_songID, songPath))
+            if (info && customIsSongDownloaded(info->m_songID, songPath)) {
+                log::debug("exists: {}", info->m_songID);
                 newSongs->addObject(obj);
+            }
         }
 
         return newSongs;
